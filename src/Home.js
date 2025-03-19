de67d3692fddee7bd6f1a40f7a9636bfa8267c40
Allan Shinhama <allanshinhama@gmail.com>
2025-03-19 20:00:49 -0300

    favicon change

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
-    <div className='Home'>
-      
+    <div className='home'>
+        {/* BEM Convention */}
+      <div className='home__container'>
+        <video src='https://m.media-amazon.com/images/I/A1QjE1HXwoL.mp4' autoPlay="true"/>
+      </div>
     </div>
   )
 }
